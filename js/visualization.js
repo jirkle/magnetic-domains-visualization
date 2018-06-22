bfd9fdc62596e1d87ac79266a7e33bcccccca493
Unknown <jiri.kletecka@gmail.com>
2018-06-22 09:08:10 +0200

    Histogram, polishing code

--- js/visualization.js
+++ js/visualization.js
@@ -85,6 +85,7 @@ function onAngleChanged() {
 	refreshMeasurements()
 	$("#measurements").change( onMeasurementChanged )
 	getAsyncData().done( function() {
+		onSelectionChanged()
 		redrawImage()
 	})
 }
@@ -93,6 +94,7 @@ function onMeasurementChanged() {
 	measurement = parseInt($("#measurements").val())
 	showLoader()
 	getAsyncData().done( function() {
+		onSelectionChanged()
 		redrawImage()
 	})
 }
@@ -238,229 +240,20 @@ function drawIntensityChart() {
 
 function drawHistogram() {
 	$('#chart').empty()
-	/*
-			values = []
-			point.each( function( index, p ) {
-				val = []
-				val.push(p["a"])
-				p["measurements"].each( function( index, m ) {
-					val.push(int(m["i"]))
-				} )
-				values.push(val)
-			} )
+	values = [['Intensity']]
+	d = imageData["rows"]
+	for (var x = selectedSquare["x1"]; x <= selectedSquare["x2"]; x++) {
+		for (var y = selectedSquare["y1"]; y <= selectedSquare["y2"]; y++) {
+			values.push([parseInt(d[y][x])])
+		}
+	}
 
-			var d = new google.visualization.DataTable();
-			d.addColumn('number', 'Angle');
-    		d.addColumn('number', 'Intensity');
-	    	d.addRows(values);
+	var d = google.visualization.arrayToDataTable(values);
+	var options = {
+        title: 'Intensity histogram',
+    	legend: { position: 'bottom' }
+    };
 
-    		var options = {
-        		title: 'Intensity based on angle',
-	        	curveType: 'function',
-    	    	legend: { position: 'bottom' }
-    		};
-
-    		var chart = new google.visualization.CandlestickChart(document.getElementById('chart'))
-			chart.draw(d, options)
-	*/
+	var chart = new google.visualization.Histogram(document.getElementById('chart'));
+    chart.draw(d, options);
 }
-
-/*
-
-function visualization() {
-	var chosenStartAngle = 0;
-	var chosenEndAngle = 0;
-	var chosenX = 500;
-	var chosenY = 500;
-	var chosenSize = 10;
-	var pixelsPerImage = 1000000;
-	var min = NaN; //Lowest value
-	var max = NaN; //Highest value
-	var color; //Color interpolator
-	var gh = 1000; // height of the image
-	var gw = 1000; //width of the image
-	
-	// Setting up canvas of the image
-	d3.select("body")
-		.select("#image")
-		.attr("width", gw)
-		.attr("height", gh)[0];
-	image = document.getElementById("image");
-
-	google.charts.load('current', {'packages':['corechart']});
-	google.charts.setOnLoadCallback(drawChart);
-
-	$( "#slider-range" ).slider({
-    	range: true,
-    	min: -20,
-    	max: 20,
-    	step: 5,
-    	values: [ 0, 0 ],
-    	slide: function( event, ui ) {
-    		chosenStartAngle = ui.values[ 0 ];
-    		chosenEndAngle = ui.values[ 1 ];
-    		refreshLegend();
-    		computeImageData();
-    		imageIt();
-    	}
-	});
-
-	$("#combine-method").change(function() {
-		computeImageData();
-		imageIt();
-	})
-
-	refreshLegend();
-	computeImageData();
-	imageIt();
-	
-	$(image).on("click", function(e) {
-		lastX = chosenX;
-		lastY = chosenY;
-		chosenX = Math.round(e.originalEvent.offsetX/this.clientWidth * this.width);
-		chosenY = Math.round(e.originalEvent.offsetY/this.clientHeight * this.height);
-		refreshLegend();
-		redrawRect(lastX - chosenSize/2 - 1, lastY - chosenSize/2 - 1,  chosenSize + 2,  chosenSize + 2);
-		drawChart();
-	});
-
-	function refreshLegend() {
-		$("#from").html(chosenStartAngle);
-    	$("#to").html(chosenEndAngle);
-    	$("#x").html(chosenX);
-    	$("#y").html(chosenY);
-	}
-	
-	function imageIt() {
-		redrawRect(0, 0, gw, gh)
-	}
-	
-	
-
-	function drawChart() {
-		pixel = 1000 * chosenY +  chosenX;
-		var d = new google.visualization.DataTable();
-		d.addColumn('number', 'Angle');
-      	d.addColumn('number', 'Intensity');
-        d.addRows([
-          [-20,  data["-20"][pixel]["intensity"]],
-          [-15,  data["-15"][pixel]["intensity"]],
-          [-10,  data["-10"][pixel]["intensity"]],
-          [-5,  data["-5"][pixel]["intensity"]],
-          [0,  data["0"][pixel]["intensity"]],
-          [5,  data["5"][pixel]["intensity"]],
-          [10,  data["10"][pixel]["intensity"]],
-          [15,  data["15"][pixel]["intensity"]],
-          [20,  data["20"][pixel]["intensity"]]
-        ]);
-
-        var options = {
-          title: 'Intensity based on angle',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-        chart.draw(d, options);
-      }
-
-	function computeImageData() {
-		len = gw * gh;
-		imageData = new Array(len);
-		min = data[chosenStartAngle][0]["intensity"];
-		max = 0;
-		if($("#combine-method").val() == "median") {
-			medianCombination();
-		} else if($("#combine-method").val() == "min") {
-			minCombination();
-		} else if($("#combine-method").val() == "max") {
-			maxCombination();
-		} else {
-			averageCombination();
-		}
-		color = d3.scale.linear()
-			.domain([min, max])
-			.range(["black", "white"])
-		
-		for (var i = 0; i < len; i++) {
-			imageData[i] = color(imageData[i]);
-		}
-	}
-
-	function averageCombination() {
-		n = (chosenEndAngle - chosenStartAngle) / 5 + 1;
-		for (var i = 0; i < len; i++) {
-			sum = 0;
-			for (var x = 0; x < n; x++) {
-				sum += data[chosenStartAngle + x * 5][i]["intensity"];
-			}
-			imageData[i] = sum / n;
-			if(imageData[i] > max) {
-				max = imageData[i];
-			} 
-			if(imageData[i] < min) {
-				min = imageData[i];
-			}
-		}
-	}
-
-	function medianCombination() {
-		n = (chosenEndAngle - chosenStartAngle) / 5 + 1;
-		for (var i = 0; i < len; i++) {
-			values = new Array(n);
-			for (var x = 0; x < n; x++) {
-				values[x] = data[chosenStartAngle + x * 5][i]["intensity"];
-			}
-			values.sort( function(a,b) {return a - b;} );
-			var half = Math.floor(values.length/2);
-
-    		if(values.length % 2)
-        		imageData[i] = values[half];
-    		else
-        		imageData[i] = (values[half-1] + values[half]) / 2.0;
-			if(imageData[i] > max) {
-				max = imageData[i];
-			} 
-			if(imageData[i] < min) {
-				min = imageData[i];
-			}
-		}
-	}
-
-	function minCombination() {
-		n = (chosenEndAngle - chosenStartAngle) / 5 + 1;
-		for (var i = 0; i < len; i++) {
-			imageData[i] = data[chosenStartAngle][i]["intensity"];
-			for (var x = chosenStartAngle + 5; x <= chosenEndAngle; x += 5) {
-				if(data[x][i]["intensity"] < imageData[i])
-					imageData[i] = data[x][i]["intensity"];
-			}
-			if(imageData[i] > max) {
-				max = imageData[i];
-			} 
-			if(imageData[i] < min) {
-				min = imageData[i];
-			}
-		}
-	}
-
-	function maxCombination() {
-		n = (chosenEndAngle - chosenStartAngle) / 5 + 1;
-		for (var i = 0; i < len; i++) {
-			imageData[i] = data[chosenStartAngle][i]["intensity"];
-			for (var x = chosenStartAngle + 5; x <= chosenEndAngle; x += 5) {
-				if(data[x][i]["intensity"] < imageData[i])
-					imageData[i] = data[x][i]["intensity"];
-			}
-			if(imageData[i] > max) {
-				max = imageData[i];
-			} 
-			if(imageData[i] < min) {
-				min = imageData[i];
-			}
-		}
-	}
-}
-*/
-
